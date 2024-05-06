779a0f685c9655b70c4997455554aba26330a36f
Alex Brandt <ajb3155@gmail.com>
2024-05-05 23:01:48 -0500

    Update models to handle user roles

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@
 
 from itsdangerous.url_safe import URLSafeTimedSerializer as Serializer
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
+from flask_login import UserMixin, AnonymousUserMixin
 from app import db
 from . import loginManager
 from flask import current_app
@@ -17,6 +17,15 @@ class User(UserMixin, db.Model):
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
     confirmed = db.Column(db.Boolean, default=False)
+
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+        
+        if self.role is None:
+            if self.email == current_app.config['FLASKY_ADMIN']:
+                self.role = Role.query.filter_by(name='Administrator').first()
+            if self.role is None:
+                self.role = Role.query.filter_by(default=True).first()
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -54,15 +63,85 @@ class User(UserMixin, db.Model):
         self.confirmed = True
         db.session.add(self)
         return True
+    
+    def can(self, perm) -> bool:
+        return self.role is not None and self.role.has_permission(perm)
+
+    def is_administrator(self) -> bool:
+        return self.can(Permission.ADMIN)
+
+class AnonymousUser(AnonymousUserMixin):
+    def can(self, permissions):
+        return False
+    
+    def is_administrator(self):
+        return False
+    
+loginManager.anonymous_user = AnonymousUser
+
+class Permission:
+    FOLLOW = 1
+    COMMENT = 2
+    WRITE = 4
+    MODERATE = 8
+    ADMIN = 16
 
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean, default=False, index=True)
+    permissions = db.Column(db.Integer)
     users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __init__(self, **kwargs):
+        super(Role, self).__init__(**kwargs)
+        if self.permissions is None:
+            self.permissions = 0
 
     def __repr__(self):
         return '<Role %r>' % self.name
+
+    def add_permission(self, perm: Permission):
+        if not self.has_permission(perm):
+            self.permissions += perm
+
+    def remove_permission(self, perm):
+        if self.has_permission(perm):
+            self.permissions -= perm
+
+    def reset_permissions(self):
+        self.permissions = 0
+
+    def has_permission(self, perm) -> bool:
+        return self.permissions & perm == perm
+
+    @staticmethod
+    def insert_roles():
+        roles = {
+            'User': [Permission.FOLLOW, Permission.COMMENT, Permission.WRITE],
+            'Moderator': [Permission.FOLLOW, Permission.COMMENT, Permission.WRITE, 
+                          Permission.MODERATE],
+            'Administrator': [Permission.FOLLOW, Permission.COMMENT, Permission.WRITE,
+                              Permission.MODERATE, Permission.ADMIN]
+        }
+
+        default_role = 'User'
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            
+            if role is None:
+                role = Role(name=r)
+                
+            role.reset_permissions()
+            
+            for perm in roles[r]:
+                role.add_permission(perm)
+                
+            role.default = (role.name == default_role)
+            db.session.add(role)
+        
+        db.session.commit()
 
 @loginManager.user_loader
 def load_user(user_id: str) -> User:
@@ -76,3 +155,4 @@ def load_user(user_id: str) -> User:
         User: User model containing data associated with user_id
     """
     return User.query.get(int(user_id))
+
--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,12 +1,25 @@
 """ Password Hashing Tests """
 
 import unittest
-from app.models import User
+from app import create_app, db
+from app.models import User, Permission, AnonymousUser, Role
 
 class UserModelTestCase(unittest.TestCase):
     """ Class to handle app.models.User test cases
     Inherited from unittest.TestCase
     """
+
+    def setUp(self):
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+        Role.insert_roles()
+        
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
 
     def test_password_setter(self):
         """ Test to validate that a user's password is hashed on initialization """
@@ -31,3 +44,18 @@ class UserModelTestCase(unittest.TestCase):
         u2 = User(password='cat')
         self.assertTrue(u.password_hash != u2.password_hash)
     
+    def test_user_role(self):
+        u = User(email='john@example.com', password='cat')
+        self.assertTrue(u.can(Permission.FOLLOW))
+        self.assertTrue(u.can(Permission.COMMENT))
+        self.assertTrue(u.can(Permission.WRITE))
+        self.assertFalse(u.can(Permission.MODERATE))
+        self.assertFalse(u.can(Permission.ADMIN))
+    
+    def test_anonymous_user(self):
+        u = AnonymousUser()
+        self.assertFalse(u.can(Permission.FOLLOW))
+        self.assertFalse(u.can(Permission.ADMIN))
+        self.assertFalse(u.can(Permission.COMMENT))
+        self.assertFalse(u.can(Permission.WRITE))
+        self.assertFalse(u.can(Permission.MODERATE))
