04efc0c1af124d2452c0bed772f88c8dad042a38
Alex Brandt <ajb3155@gmail.com>
2024-05-20 00:42:17 -0500

    Updated start file to include permissions class

--- flasky.py
+++ flasky.py
@@ -1,18 +1,25 @@
 import os
+import click
 from flask_migrate import Migrate
 from app import create_app, db
-from app.models import Role, User
+from app.models import Role, User, Permission
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(db=db, User=User, Role=Role)
+    return dict(db=db, User=User, Role=Role, Permission=Permission)
 
 @app.cli.command()
-def test():
+@click.argument('test_names', nargs=-1)
+def test(test_names):
     """Run the unit tests."""
     import unittest
-    tests = unittest.TestLoader().discover('tests')
+
+    if test_names:
+        tests = unittest.TestLoader().discover(test_names)
+    else:
+        tests = unittest.TestLoader().discover('tests')
+
     unittest.TextTestRunner(verbosity=2).run(tests)
